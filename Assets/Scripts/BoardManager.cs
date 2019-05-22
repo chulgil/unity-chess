75366330662f2148fab73f27640cda09143e8da6
CG.Lee <contact@chulgil.me>
2019-05-22 17:31:16 +0900

    add assets

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    private const float TILE_SIZE = 1.0f;
+    private const float TILE_OFFSET = 0.5f;
+
+    private int selectionX = -1;
+    private int selectionY = -1;
+
+    public List<GameObject> chessmanPrefabs;
+    private List<GameObject> activeChessman = new List<GameObject>();
+
+    private void Start() {
+        SpawnChessman(0, GetTileCenter(3,0));
+    }
+    private void Update() {
+        UpdateSelection();
+        DrowChessboard();
+    }
+
+    private void UpdateSelection()
+    {
+        if(!Camera.main) 
+            return;
+        
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane")))
+        {
+            Debug.Log(hit.point);
+            selectionX = (int)hit.point.x;
+            selectionY = (int)hit.point.z;
+        }
+        else
+        {
+            selectionX = -1;
+            selectionY = -1;
+        }
+
+    }
+
+    private void SpawnChessman(int index, Vector3 position)
+    {
+        GameObject go = Instantiate(chessmanPrefabs[index], position, Quaternion.identity) as GameObject;
+        go.transform.SetParent(transform);
+        activeChessman.Add(go);
+    }
+
+    private Vector3 GetTileCenter(int x, int y)
+    {
+        Vector3 origin = Vector3.zero;
+        origin.x += (TILE_SIZE * x) + TILE_OFFSET;
+        origin.z+= (TILE_SIZE * y) + TILE_OFFSET;
+        return origin;
+    }
+
+    private void DrowChessboard()
+    {
+        Vector3 xLine = Vector3.right * 8;
+        Vector3 yLine = Vector3.forward * 8;
+        for(int i = 0; i <= 8; i++)
+        {
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + xLine);
+
+            for(int y = 0; y <= 8; y++)
+            {
+                start = Vector3.right * i;
+                Debug.DrawLine(start, start + yLine);
+            }   
+        }
+
+        // Draw the selection
+        if(selectionX >= 0 && selectionY >= 0)
+        {
+            Debug.DrawLine(
+                Vector3.forward * selectionY + Vector3.right * selectionX,
+                Vector3.forward * (selectionY + 1) +  Vector3.right * (selectionX + 1));
+            Debug.DrawLine(
+                Vector3.forward * (selectionY + 1) + Vector3.right * selectionX,
+                Vector3.forward * selectionY + Vector3.right * (selectionX + 1));
+            
+        }
+    }
+}
